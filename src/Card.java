e3f64d92fba3a93cb99c017f107f6f6c7f94a110
Domou Brice <domoubrice@gmail.com>
2023-08-17 07:47:38 +0100

    using classes

--- src/Card.java
+++ src/Card.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Card {
+    private static final int unitPrice = 8;
+
     private int numberOfItems;
     private List<Book> bookList = new ArrayList<>();
 
@@ -27,6 +32,171 @@ public class Card {
 
     public void setBookList(List<Book> bookList) {
         this.bookList = bookList;
+    }
+
+    public double calculateBasketCost(List<Book> bookList) {
+
+        List<Integer> series = new ArrayList<>();
+        for (Book book : bookList) {
+            series.add(book.getQuantity());
+        }
+        List<String> combinationList = new ArrayList<>();
+        List<Double> combinationDiscountList = new ArrayList<>();
+
+
+        List<Integer> initialSeries = series;
+
+        System.out.println("number of items: " + numberOfItems);
+
+        int nextNumberGrouped = series.size();
+        int numberGrouped = series.size();
+        int initialNumberOfSeriesType  = series.size();
+        int numberOfSeriesTypeConsidered = series.size();
+        double discountValue = 0;
+
+
+        while (numberOfSeriesTypeConsidered >= 1) {
+            System.out.println("GROUP BY " + numberGrouped);
+            String combination = String.valueOf(numberGrouped);
+
+            while (numberGrouped > 0) {
+                ArrayList<Integer> tempSeries = new ArrayList<>();
+
+                if (numberGrouped != 1) {
+
+
+                    series.removeAll(Collections.singleton(0));
+                    Collections.sort(series);
+                    System.out.println(series);
+                    int ite = 0;
+                    if (nextNumberGrouped == 5 && series.size()>0) {
+                        for (int i = series.size()-1; i >= 0; i--) {
+                            tempSeries.add(series.get(i) - 1);
+                        }
+                        System.out.println(tempSeries);
+                    }
+
+                    if (nextNumberGrouped == 4 && series.size()>0) {
+                        for (int i = series.size()-1; i >= 0; i--) {
+                            tempSeries.add(series.get(i) - 1);
+                            System.out.println(tempSeries.size());
+                        }
+                        System.out.println(tempSeries.size());
+                        if (series.size()>4) {
+                            tempSeries.remove(series.size()-1);
+                            tempSeries.add(series.get(0));
+                        }
+                        System.out.println(tempSeries.size());
+                    }
+
+                    if (nextNumberGrouped == 3 && series.size()>0) {
+                        for (int i = series.size()-1; i >= 0; i--) {
+                            tempSeries.add(series.get(i) - 1);
+                            System.out.println(tempSeries.size());
+                        }
+                        if (series.size()==5) {
+                            tempSeries.remove(series.size()-1);
+                            tempSeries.remove(series.size()-2);
+
+                            tempSeries.add(series.get(0));
+                            tempSeries.add(series.get(1));
+                        }
+                        if (series.size()==4) {
+                            tempSeries.remove(series.size()-1);
+
+                            tempSeries.add(series.get(0));
+                        }
+                        System.out.println(tempSeries.size());
+                    }
+
+                    if (nextNumberGrouped == 2 && series.size()>0) {
+                        for (int i = series.size()-1; i >= 0; i--) {
+                            tempSeries.add(series.get(i) - 1);
+                            System.out.println(tempSeries.size());
+                        }
+                        if (series.size()==5) {
+                            tempSeries.remove(series.size()-1);
+                            tempSeries.remove(series.size()-2);
+                            tempSeries.remove(series.size()-3);
+
+                            tempSeries.add(series.get(0));
+                            tempSeries.add(series.get(1));
+                            tempSeries.add(series.get(2));
+                        }
+                        if (series.size()==4) {
+                            tempSeries.remove(series.size()-1);
+                            tempSeries.remove(series.size()-2);
+                            tempSeries.add(series.get(0));
+                            tempSeries.add(series.get(1));
+                        }
+                        if (series.size()==3) {
+                            tempSeries.remove(series.size()-1);
+
+                            tempSeries.add(series.get(0));
+                        }
+                        System.out.println(tempSeries.size());
+                    }
+
+                    tempSeries.removeAll(Collections.singleton(0));
+
+                    System.out.println(numberGrouped);
+                    discountValue += calculateDiscountedPrice(numberGrouped);
+                    System.out.println(tempSeries.size());
+                    System.out.println(discountValue);
+                } else if (nextNumberGrouped == 1) {
+                    discountValue = numberOfItems * calculateDiscountedPrice(1);
+                } else {
+
+                    discountValue += series.get(0) * calculateDiscountedPrice(1);
+                }
+
+                series = tempSeries;
+                System.out.println(series);
+                numberGrouped = numberGrouped > series.size() ? series.size() : nextNumberGrouped > numberGrouped ? numberGrouped : nextNumberGrouped;
+                System.out.println(numberGrouped);
+                combination += (numberGrouped == 1) ? "-" + numberGrouped + "(" + series.get(0) + ")" : "-" + numberGrouped;
+
+                System.out.println(Arrays.toString(new List[]{series}));
+
+            }
+            System.out.println(discountValue);
+            combinationList.add(combination);
+            combinationDiscountList.add(discountValue);
+            discountValue = 0;
+
+            numberOfSeriesTypeConsidered = numberOfSeriesTypeConsidered-1;
+            series = initialSeries;
+
+            numberGrouped = numberOfSeriesTypeConsidered;
+            nextNumberGrouped = numberOfSeriesTypeConsidered;
+
+
+        }
+
+
+        for (double discVal: combinationDiscountList) {
+            System.out.println("Discount Combination  " + combinationList.get(combinationDiscountList.indexOf(discVal)) + " : " + discVal );
+        }
+
+        List<Double> backupDiscountList = combinationDiscountList;
+        Collections.sort(backupDiscountList);
+        System.out.println("The best discount combination is: ");
+        System.out.println(combinationList.get(combinationDiscountList.indexOf(backupDiscountList.get(0))) + ": " + backupDiscountList.get(0));
+
+        return backupDiscountList.get(0);
+
+    }
+
+    public static double calculateDiscountedPrice(int number) {
+        double rate;
+        switch (number){
+            case 2 -> rate = 0.05;
+            case 3 -> rate = 0.1;
+            case 4 -> rate = 0.2;
+            case 5 -> rate = 0.25;
+            default -> rate = 0;
+        }
+        return number*(unitPrice - unitPrice*rate);
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
     private static final Scanner scanner = new Scanner(System.in);
-    private static final int unitPrice = 8;
+//    private static final int unitPrice = 8;
     public static void main(String[] args) {
         Book s1 = new Book("B1","JAVA for beginners");
         Book s2 = new Book("B2","JVM: Explore the Java Virtual Machine in depth");
@@ -38,145 +39,5 @@ public class Main {
         card.setBookList(Arrays.asList(s1, s2, s3, s4, s5));
         System.out.println(card);
 
-        ArrayList<Integer> series = new ArrayList<>();
-        ArrayList<String> combinationList = new ArrayList<>();
-        ArrayList<Double> combinationDiscountList = new ArrayList<>();
-
-        for (Book book : card.getBookList()) {
-            if (book.getQuantity() != 0) {
-                series.add(book.getQuantity());
-            }
-        }
-
-        ArrayList<Integer> initialSeries = series;
-//        for (Integer integer1 : series) {
-//            numberOfItems = numberOfItems + integer1;
-//        }
-        System.out.println("number of items: " + numberOfItems);
-
-        int nextNumberGrouped = series.size();
-        int numberGrouped = series.size();
-        int initialNumberOfSeriesType  = series.size();
-        int numberOfSeriesTypeConsidered = series.size();
-        double discountValue = 0;
-
-        while (numberOfSeriesTypeConsidered >= 1) {
-            System.out.println("GROUP BY " + numberGrouped);
-            String combination = String.valueOf(numberGrouped);
-
-            while (numberGrouped > 0) {
-                ArrayList<Integer> tempSeries = new ArrayList<>();
-
-                if (numberGrouped != 1) {
-
-                    Collections.sort(series);
-                    System.out.println(series);
-                    int ite = 0;
-                    for (int i = series.size()-1; i >= 0; i--) {
-                            if (ite < numberGrouped && series.get(i) > 1) {
-                                tempSeries.add(series.get(i) - 1);
-                            }
-                        System.out.println(tempSeries);
-                            if (ite >= numberGrouped) {
-//                            System.out.println("ite = " + ite);
-//                                System.out.println("tempSeries size = " + tempSeries.size());
-
-                                if ( ite >= tempSeries.size() ) {
-                                    switch(ite) {
-                                        case 4:
-                                            if (series.size() > 4) {
-                                                tempSeries.add(series.get(0));
-                                            }
-                                            break;
-                                        case 3:
-                                            if (series.size() > 3) {
-                                                tempSeries.add(series.get(0));
-                                                tempSeries.add(series.get(1));
-                                            }
-                                            break;
-                                        case 2:
-                                            if (series.size() > 3) {
-                                                tempSeries.add(series.get(0));
-                                                tempSeries.add(series.get(1));
-                                                tempSeries.add(series.get(2));
-                                            }
-                                        default:
-                                            break;
-                                    }
-
-                                }
-
-                                break;
-                            }
-
-                        ite++;
-                        }
-
-                    discountValue += calculateDiscountedPrice(numberGrouped);
-                } else if (nextNumberGrouped == 1) {
-                    discountValue = numberOfItems * calculateDiscountedPrice(1);
-                } else {
-
-                    discountValue += series.get(0) * calculateDiscountedPrice(1);
-                }
-
-                series = tempSeries;
-                numberGrouped = numberGrouped > series.size() ? series.size() : nextNumberGrouped;
-                combination += "-" + numberGrouped;
-
-            System.out.println(Arrays.toString(new ArrayList[]{series}));
-
-            }
-            System.out.println(discountValue);
-            combinationList.add(combination);
-            combinationDiscountList.add(discountValue);
-            discountValue = 0;
-
-            numberOfSeriesTypeConsidered = numberOfSeriesTypeConsidered-1;
-            series = initialSeries;
-
-            numberGrouped = numberOfSeriesTypeConsidered;
-            nextNumberGrouped = numberOfSeriesTypeConsidered;
-
-
-        }
-
-//        for (String disc: combinationList) {
-//            System.out.println("Discount Combination:  " + disc );
-//        }
-
-        for (double discVal: combinationDiscountList) {
-            System.out.println("Discount Combination  " + combinationList.get(combinationDiscountList.indexOf(discVal)) + " : " + discVal );
-        }
-
-        ArrayList<Double> backupDiscountList = combinationDiscountList;
-        Collections.sort(backupDiscountList);
-        System.out.println("The best discount combination is: ");
-        System.out.println(combinationList.get(combinationDiscountList.indexOf(backupDiscountList.get(0))) + ": " + backupDiscountList.get(0));
-
-    }
-
-
-    public static double calculateDiscountedPrice(int number) {
-        double rate;
-        switch (number){
-            case 2:
-                rate = 0.05;
-                break;
-            case 3:
-                rate = 0.1;
-                break;
-            case 4:
-                rate = 0.2;
-                break;
-            case 5:
-                rate = 0.25;
-                break;
-            default:
-                rate = 0;
-                break;
-    }
-    double discount = number*(unitPrice - unitPrice*rate);
-        return discount;
-    }
-}
+        card.calculateBasketCost(card.getBookList());
+}}
